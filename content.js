bb15e09bc6be51b84cf6d500add26bd3ecd2aae0
pa.solodov <pa.solodov@tensor.ru>
2017-01-30 12:07:16 +0300

    creation

---
+++ content.js
@@ -0,0 +1,27 @@
+(function () {
+   var
+         albumMatch = document.URL.match(/Album\/[\d]+/g),
+   // упорядочивание по убыванию
+         compare = function (a, b) {
+            return a > b ? -1 : a < b ? 1 : 0;
+         };
+
+   if (albumMatch && albumMatch[0]) {
+      var links = document.querySelectorAll('a[href^="/Song/"]');
+      for(var i = 0, len = links.length; i < len; i++) {
+         var link = links[i];
+         if (link.innerText === 'СКАЧАТЬ') {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function (link) {
+               if (this.readyState == 4 && this.status == 200) {
+                  var href = this.responseText.match(/\/Song\/Download\/[\w\d\?=&;]+/);
+                  href = href && href[0] && href[0].replace('amp;','');
+                  href && (link.href = href) && (link.style = 'color: green;');
+               }
+            }.bind(xhttp, link);
+            xhttp.open('GET', link.href, true);
+            xhttp.send();
+         }
+      }
+   }
+})();
