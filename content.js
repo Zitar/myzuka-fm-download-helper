9731650a5cc5730c59fa01228390d3ca92034ef1
zitar <vanzitar@gmail.com>
2017-12-17 19:17:58 +0300

    new structure for easy extending for similar sites

--- content.js
+++ content.js
@@ -1,30 +1,57 @@
 (function () {
    var
-         albumMatch = document.URL.match(/Album\/[\d]+/g),
-   // упорядочивание по убыванию
-         compare = function (a, b) {
-            return a > b ? -1 : a < b ? 1 : 0;
-         };
-
-   if (albumMatch && albumMatch[0]) {
-      var links = document.querySelectorAll('a[href^="/Song/"]');
-      for(var i = 0, len = links.length; i < len; i++) {
-         var link = links[i];
-         if (link.innerText === 'СКАЧАТЬ') {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function (link) {
-               if (this.readyState == 4 && this.status == 200) {
-                  var href = this.responseText.match(/\/Song\/Download\/[\w\d\?=&;]+/);
-                  href = href && href[0] && href[0].replace('amp;','');
-                  href && (link.href = href) && (link.style = 'color: green;');
-                  link.onclick = function () {
-                     this.style = 'color: red;';
-                  }.bind(link);
-               }
-            }.bind(xhttp, link);
-            xhttp.open('GET', link.href, true);
-            xhttp.send();
+      units = [
+         {
+            urlRegExp: /Album\/[\d]+/g,
+            linksSelector: 'a[href^="/Song/"]',
+            linkCheck: function (link) {
+               return link.innerText === 'СКАЧАТЬ';
+            },
+            readyResultCheck: function (xhttp) {
+               return xhttp.readyState === 4;
+            },
+            downloadHrefRegExp: /\/Song\/Download\/[\w\d\?=&;]+/,
+            hrefModify: function (href) {
+               return href && href[0] && href[0].replace('amp;','');
+            }
+         }, {
+            urlRegExp: /release\/[\d]+\/[_\w\d]+\//g,
+            linksSelector: 'a[href^="/track/"]',
+            linkCheck: function (link) {
+               return link.firstChild && link.firstChild.tagName === 'IMG';
+            },
+            downloadHrefRegExp: /\/freedownload\.php\?id=[\d]+/,
          }
+      ],
+      unit,
+      urlMatch,
+      links,
+      link;
+
+   for (var i = 0, len = units.length; i < len; i++) {
+      unit = units[i];
+      urlMatch = document.URL.match(unit.urlRegExp);
+      if (urlMatch && urlMatch[0]) {
+         links = document.querySelectorAll(unit.linksSelector);
+         for(var j = 0, linksLen = links.length; j < linksLen; j++) {
+            link = links[j];
+            if (unit.linkCheck === undefined || unit.linkCheck(link)) {
+               var xhttp = new XMLHttpRequest();
+               xhttp.onreadystatechange = function (unit, link) {
+                  if (this.status === 200 && (unit.readyResultCheck === undefined || unit.readyResultCheck(this))) {
+                     var href = this.responseText.match(unit.downloadHrefRegExp);
+                     href = unit.hrefModify ? unit.hrefModify(href) : href;
+                     href && (link.href = href) && (link.innerHTML = 'Скачать') && (link.style = 'color: green;');
+                     link.onclick = function () {
+                        this.style = 'color: red;';
+                     }.bind(link);
+                  }
+               }.bind(xhttp, unit, link);
+               xhttp.open('GET', link.href, true);
+               xhttp.send();
+            }
+         }
+         break;
       }
    }
 })();
